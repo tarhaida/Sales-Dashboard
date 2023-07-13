f2c4b1f30ca5499a0ced369b1164371d7e89f5f8
tarhaida <139452301+tarhaida@users.noreply.github.com>
2023-07-13 12:22:36 +0200

    Add files via upload

--- Dashboard.py
+++ Dashboard.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import matplotlib.pyplot as plt
-import os
-import warnings
-warnings.filterwarnings('ignore')
+#import warnings
+#warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title="Dashboard Sales", page_icon=":bar_chart:",layout="wide")
 
