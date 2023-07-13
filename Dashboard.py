9bc78e6384f84dec5c599d8ba4f9f6290362bf5f
tarhaida <139452301+tarhaida@users.noreply.github.com>
2023-07-13 12:40:29 +0200

    Add files via upload

--- Dashboard.py
+++ Dashboard.py
@@ -1,22 +1,12 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-#import warnings
-#warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title="Dashboard Sales", page_icon=":bar_chart:",layout="wide")
 
 st.title(" :bar_chart: Sample Brand Store")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 
-#fl = st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
-
-#if fl is not None:
-#    filename = fl.name
-#    st.write(filename)
-#    df = pd.read_csv(filename, encoding = "ISO-8859-1")
-#else:
-    #os.chdir(r"C:\Users\AEPAC\Desktop\Streamlit")
 df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
 
 col1, col2 = st.columns((2))
